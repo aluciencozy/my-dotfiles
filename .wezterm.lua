6cd87458dd251d8038121141638fbebf142c7e45
aluciencozy <aluciencozy22@gmail.com>
2026-07-14 22:10:44 -0400

    Update WezTerm configuration

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,7 @@
 ﻿local wezterm = require("wezterm")
+local mux = wezterm.mux
 local config = wezterm.config_builder()
 
--- Your aesthetic setups
 config.color_scheme = "rose-pine-moon"
 config.font = wezterm.font("JetBrains Mono", { weight = 'Bold' })
 config.font_size = 15.0
@@ -10,13 +10,13 @@ config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
-config.initial_cols = 180
-config.initial_rows = 41
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
+end)
 
--- Automatically boot straight into Herdr inside WSL Ubuntu
 config.default_prog = { "wsl.exe", "-d", "Ubuntu", "/home/alex/.local/bin/herdr" }
 
--- Kept empty so WezTerm lets Herdr handle all your splits and tabs
 config.keys = {}
 
 return config
