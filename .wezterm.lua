d0f9e8d73b08c6856df65bd8d350f4f27f2f53f3
aluciencozy <aluciencozy22@gmail.com>
2026-07-16 22:46:38 -0400

    Update WezTerm configuration

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,22 +1,38 @@
 ﻿local wezterm = require("wezterm")
-local mux = wezterm.mux
 local config = wezterm.config_builder()
 
 config.color_scheme = "rose-pine-moon"
-config.font = wezterm.font("JetBrains Mono", { weight = 'Bold' })
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
 config.font_size = 15.0
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 50
+
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
+config.window_background_opacity = 0.80
+config.win32_system_backdrop = "Acrylic"
+
+config.default_prog = {
+  "wsl.exe",
+  "-d",
+  "Ubuntu",
+  "/home/alex/.local/bin/herdr",
+}
+
 wezterm.on("gui-startup", function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():toggle_fullscreen()
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
-config.default_prog = { "wsl.exe", "-d", "Ubuntu", "/home/alex/.local/bin/herdr" }
-
 config.keys = {}
+
+config.bypass_mouse_reporting_modifiers = "ALT"
+
+config.mouse_bindings = {
+  {
+    event = { Drag = { streak = 1, button = "Left" } },
+    mods = "NONE",
+    action = wezterm.action.StartWindowDrag,
+  },
+}
 
 return config
